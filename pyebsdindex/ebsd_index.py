013048b0688e209c806417eeac61ea3b40312635
David Rowenhorst <david.rowenhorst@nrl.navy.mil>
2021-11-15 14:00:45 -0500

    Cleanup of ebsd_index -- removed multiprocess indexing leaving only Ray distributed.
    Signed-off by: David Rowenhorst <david.rowenhorst@nrl.navy.mil>

--- pyebsdindex/ebsd_index.py
+++ pyebsdindex/ebsd_index.py
@@ -69,141 +69,6 @@ def index_pats(patsIn=None,filename=None,filenameout=None,phaselist=['FCC'], \
   else:
     return dataout,indexer
 
-
-@ray.remote(num_cpus=1,num_gpus=1)
-class IndexerRay():
-  def __init__(self,actorid=0):
-    sys.path.append(path.dirname(__file__))  # do this to help Ray find the program files
-    import openclparam # do this to help Ray find the program files
-    # device, context, queue, program, mf
-    # self.dataout = None
-    # self.indxstart = None
-    # self.indxend = None
-    # self.rate = None
-    self.actorID = actorid
-    self.openCLParams = None
-    try:
-      if sys.platform != 'darwin':  # linux with NVIDIA (unsure if it is the os or GPU type) is slow to make a
-        self.openCLParams = openclparam.OpenClParam()
-        self.openCLParams.gpu_id = self.actorID % self.openCLParams.ngpu
-
-      else:  # MacOS handles GPU memory conflicts much better when the context is destroyed between each
-        # run, and has very low overhead for making the context.
-        #pass
-        self.openCLParams = openclparam.OpenClParam()
-        #self.openCLParams.gpu_id = 0
-        # self.openCLParams.gpu_id = 1
-        self.openCLParams.gpu_id = self.actorID % self.openCLParams.ngpu
-    except:
-      self.openCLParams = None
-
-  def index_chunk_ray(self,pats=None,indexer=None,patstart=0,npats=-1):
-    try:
-      tic = timer()
-      dataout,indxstart,npatsout = indexer.index_pats(patsin=pats,patstart=patstart,npats=npats,
-                                                      clparams=self.openCLParams)
-      rate = np.array([timer() - tic,npatsout])
-      return dataout,indxstart,indxstart + npatsout,rate
-    except:
-      indxstart = patstart
-      indxend = patstart + npats
-      return None,indxstart,indxend,[-1,-1]
-  # def readdata(self):
-  #   return self.dataout, self.indxstart,self.indxend, self.rate
-  #
-  # def cleardata(self):
-  #   #self.dataout = None
-  #   self.indxstart = None
-  #   self.indxend = None
-  #   self.rate = None
-
-
-def index_chunk_MP(pats=None,queues=None,lock=None,id=None):
-  # queues[0]: messaging queue -- will initially get the indexer object, and the mode.  Will listen for a "Done" after that.
-  # queues[1]: job queue
-  # queues[2]: results queue
-
-  indexer,mode = queues[0].get()
-  workToDo = True
-  while workToDo:
-    tic = timer()
-
-    try:
-      # lock.acquire()
-      if mode == 'filemode':
-        pats = None
-        patStart,patEnd,jid = queues[1].get(False)
-      elif mode == 'memory':  # the patterns were stored in memory and sent within the job queue
-        pats,patStart,patEnd,jid = queues[1].get(False)
-    except queue.Empty:  # apparently queue.Empty is really unreliable.
-      # lock.release()
-      time.sleep(0.01)
-    else:
-      # lock.release()
-      dataout,indxstart,indxend = indexer.index_pats(patsin=pats,patStart=patStart,patEnd=patEnd)
-      rate = np.array([timer() - tic,indxend - indxstart])
-
-      # try:
-      queues[2].send((dataout,indxstart,indxend,rate,id,jid))
-      # except:
-      #  print("Unexpected error:",sys.exc_info()[0])
-
-    # We will wait until an explicit "Done" comes from the main thread.
-    try:
-      wait_for_quit = queues[0].get(False)
-      if wait_for_quit == 'Done':
-        workToDo = False
-        queues[2].close()
-        return
-    except queue.Empty:
-      time.sleep(0.01)
-  return
-
-
-def index_chunk_MP2(pats=None,queues=None,lock=None,id=None):
-  # queues[0]: messaging queue -- will initially get the indexer object, and the mode.  Will listen for a "Done" after that.
-  # queues[1]: job queue
-  # queues[2]: results queue
-
-  indexer,mode = queues[0].get()
-  # print('here', id)
-  workToDo = True
-  while workToDo:
-    tic = timer()
-    lock[0].acquire()
-    # print(queues[1].empty())
-    if queues[1].empty() == False:
-      if mode == 'filemode':
-        pats = None
-        patStart,patEnd,jid = queues[1].get()
-      elif mode == 'memory':  # the patterns were stored in memory and sent within the job queue
-        pats,patStart,patEnd,jid = queues[1].get()
-      lock[0].release()
-      dataout,indxstart,indxend = indexer.index_pats(patsin=pats,patStart=patStart,patEnd=patEnd)
-      rate = np.array([timer() - tic,indxend - indxstart])
-
-      queues[2].send((dataout,indxstart,indxend,rate,id,jid))
-    else:
-      lock[0].release()
-      time.sleep(0.01)
-
-    # We will wait until an explicit "Done" comes from the main thread.
-    lock[1].acquire()
-    if queues[0].empty() == False:
-      wait_for_quit = queues[0].get()
-      lock[1].release()
-      if wait_for_quit == 'Done':
-        workToDo = False
-        queues[2].close()
-        # queues[0].close()
-
-        return
-    else:
-      lock[1].release()
-      time.sleep(0.01)
-  return
-
-
 def index_pats_distributed(patsIn=None,filename=None,filenameout=None,phaselist=['FCC'], \
                            vendor=None,PC=None,sampleTilt=70.0,camElev=5.3, \
                            peakDetectPlan=None,nRho=90,nTheta=180,tSigma=None,rSigma=None,rhoMaskFrac=0.1,nBands=9,
@@ -491,187 +356,44 @@ def index_pats_distributed(patsIn=None,filename=None,filenameout=None,phaselist=
   else:
     return dataout
 
+@ray.remote(num_cpus=1,num_gpus=1)
+class IndexerRay():
+  def __init__(self,actorid=0):
+    sys.path.append(path.dirname(__file__))  # do this to help Ray find the program files
+    import openclparam # do this to help Ray find the program files
+    # device, context, queue, program, mf
+    # self.dataout = None
+    # self.indxstart = None
+    # self.indxend = None
+    # self.rate = None
+    self.actorID = actorid
+    self.openCLParams = None
+    try:
+      if sys.platform != 'darwin':  # linux with NVIDIA (unsure if it is the os or GPU type) is slow to make a
+        self.openCLParams = openclparam.OpenClParam()
+        self.openCLParams.gpu_id = self.actorID % self.openCLParams.ngpu
 
-def index_patsMP(pats=None,filename=None,filenameout=None,phaselist=['FCC'], \
-                 vendor=None,PC=None,sampleTilt=70.0,camElev=5.3, \
-                 peakDetectPlan=None,nRho=90,nTheta=180,tSigma=None,rSigma=None,rhoMaskFrac=0.1,nBands=9,
-                 patStart=0,patEnd=-1,chunksize=1000,ncpu=-1,
-                 return_indexer_obj=False,ebsd_indexer_obj=None):
-  n_cpu_nodes = int(multiprocessing.cpu_count())  # int(sum([ r['Resources']['CPU'] for r in ray.nodes()]))
-  if ncpu != -1:
-    n_cpu_nodes = ncpu
+      else:  # MacOS handles GPU memory conflicts much better when the context is destroyed between each
+        # run, and has very low overhead for making the context.
+        #pass
+        self.openCLParams = openclparam.OpenClParam()
+        #self.openCLParams.gpu_id = 0
+        #self.openCLParams.gpu_id = 1
+        self.openCLParams.gpu_id = self.actorID % self.openCLParams.ngpu
+    except:
+      self.openCLParams = None
 
-  if pats is None:
-    pdim = None
-  else:
-    pdim = pats.shape[-2:]
-  if ebsd_indexer_obj == None:
-    indexer = EBSDIndexer(filename=filename,phaselist=phaselist, \
-                          vendor=None,PC=PC,sampleTilt=sampleTilt,camElev=camElev, \
-                          bandDetectPlan=peakDetectPlan, \
-                          nRho=nRho,nTheta=nTheta,tSigma=tSigma,rSigma=rSigma,rhoMaskFrac=rhoMaskFrac,nBands=nBands,
-                          patDim=pdim)
-  else:
-    indexer = ebsd_indexer_obj
-
-  # differentiate between getting a file to index or an array
-  # Need to index one pattern to make sure the indexer object is fully initiated before
-  #   placing in shared memory store.
-  mode = 'memorymode'
-  if pats is None:
-    mode = 'filemode'
-    # just make sure indexer is fully initialized
-    temp,indexer = index_pats(patStart=0,patEnd=1,return_indexer_obj=True,ebsd_indexer_obj=indexer)
-
-  if mode == 'filemode':
-    npats = indexer.fID.nPatterns
-  else:
-    pshape = pats.shape
-    if len(pshape) == 2:
-      npats = 1
-      pats = pats.reshape([1,pshape[0],pshape[1]])
-    else:
-      npats = pshape[0]
-    temp,indexer = index_pats(pats[0,:,:],patStart=0,patEnd=1,return_indexer_obj=True,ebsd_indexer_obj=indexer)
-
-  if patEnd == 0:
-    patEnd = 1
-  if (patStart != 0) or (patEnd > 0):
-    npats = patEnd - patStart
-
-  # set up the jobs
-  njobs = (np.ceil(npats / chunksize)).astype(np.long)
-  p_indx_start = [i * chunksize + patStart for i in range(njobs)]
-  p_indx_end = [(i + 1) * chunksize + patStart for i in range(njobs)]
-  p_indx_end[-1] = npats + patStart
-  if njobs < n_cpu_nodes:
-    n_cpu_nodes = njobs
-
-  dataout = np.zeros((npats),dtype=indexer.dataTemplate)
-  returnqueue = []
-
-  ndone = 0
-  tic = timer()
-  npatsdone = 0.0
-  toc = 0.0
-  messagequeue = []  # each worker will get its own message queue
-  messagelock = []
-  workers = []
-  rateave = 0.0
-  readlock = multiprocessing.Lock()
-  jobqueue = multiprocessing.SimpleQueue()
-  jcheck = np.ones(njobs)
-  if mode == 'filemode':
-
-    tic = timer()
-    for i in range(n_cpu_nodes):  # launch the workers
-      messagequeue.append(multiprocessing.SimpleQueue())
-      messagelock.append(multiprocessing.Lock())
-      returnqueue.append(multiprocessing.Pipe())
-      workers.append(multiprocessing.Process(target=index_chunk_MP2,args=(None, \
-                                                                          [messagequeue[i],jobqueue,returnqueue[i][1]],
-                                                                          [readlock,messagelock[i]],i)))
-      workers[i].start()
-
-    readlock.acquire()
-    for i in range(njobs):
-      jobqueue.put((p_indx_start[i],p_indx_end[i],i))
-    readlock.release()
-    for i in range(n_cpu_nodes):
-      messagelock[i].acquire()
-      messagequeue[i].put((indexer,mode))
-      messagelock[i].release()
-    ndone = 0
-    ntry = -1
-    tryThresh = 400
-    ntrysum = 0
-    while ndone < njobs:
-      # check if work is done.
-
-      for i in range(n_cpu_nodes):
-        # try:
-        if (returnqueue[i][0]).poll():
-          wrkdataout,indxstr,indxend,rate,wrkID,jid = returnqueue[i][0].recv()
-          dataout[indxstr:indxend] = wrkdataout
-          npatsdone += rate[1]
-          ratetemp = n_cpu_nodes * (rate[1]) / (rate[0])
-          rateave += ratetemp
-          jcheck[jid] = 0
-          ndone += 1  # np.count_nonzero(jcheck == 0)
-          ntrysum += np.max((ntry,0))
-          tryThresh = np.int(ntrysum / np.float(ndone))
-          # print( tryThresh, ntry)
-          ntry = 0
-          print('Completed: ',str(indxstr),' -- ',str(indxend),'  ',np.int(ratetemp),'  ',
-                np.int(npatsdone / (timer() - tic)),np.int(npatsdone / npats * 100),'%  ',njobs,ndone)
-          whzero = jcheck.nonzero()[0]
-          # if whzero.size < 10:
-          #  print(jcheck.nonzero()[0])
-        # except queue.Empty: # there is a strange race condition that I can not figure out.
-        # else:
-      time.sleep(0.005)
-      ntry += 1
-      if ntry > (10 * tryThresh):
-        ntry = 0
-        whbad = jcheck.nonzero()[0]
-        if whbad.size > 0:
-          print('adding back job',whbad[0])
-          jobqueue.put((p_indx_start[whbad[0]],p_indx_end[whbad[0]],whbad[0]))
-        # except:
-        #  print("Unexpected error:",sys.exc_info()[0])
-
-  if mode == 'memorymode':
-
-    nsubmit = 0
-    n2queue = n_cpu_nodes + 4 if (njobs > (n_cpu_nodes + 4)) else n_cpu_nodes  # going to pre-load a few extra jobs.
-    # memory concerns are the only reason we do not load all into the queue at once.
-    for i in range(n2queue):
-      jobqueue.put(((pats[p_indx_start[nsubmit]:p_indx_end[nsubmit],:,:],p_indx_start[nsubmit],p_indx_end[nsubmit])))
-      nsubmit += 1
-
-    for i in range(n_cpu_nodes):  # launch the workers
-      toc = timer()
-      messagequeue.append(multiprocessing.Queue())
-      # print('New q', timer()-toc)
-      toc = timer()
-      messagequeue[i].put((indexer,mode))  # provide the indexer and where the patterns are coming from
-      workers.append(multiprocessing.Process(target=index_chunk_MP,args=(None, \
-                                                                         [messagequeue[i],jobqueue,returnqueue],i)))
-      workers[i].start()
-
-    while ndone < njobs:
-      # check if work is done.
-      try:
-        wrkdataout,indxstr,indxend,rate,wrkID = returnqueue.get(False)
-        dataout[indxstr:indxend] = wrkdataout
-        npatsdone += rate[1]
-        ratetemp = n_cpu_nodes * (rate[1]) / (rate[0])
-        rateave += ratetemp
-        ndone += 1
-        print('Completed: ',str(indxstr),' -- ',str(indxend),'  ',np.int(ratetemp),'  ',
-              np.int(npatsdone / (timer() - tic)),np.int(npatsdone / npats * 100),'%  ',njobs,ndone)
-        if nsubmit < njobs:  # still more to do.
-          jobqueue.put(
-            ((pats[p_indx_start[nsubmit]:p_indx_end[nsubmit],:,:],p_indx_start[nsubmit],p_indx_end[nsubmit])))
-          nsubmit += 1
-      except queue.Empty:
-        pass
-
-  for i in range(n_cpu_nodes):
-    messagelock[i].acquire()
-    messagequeue[i].put('Done')
-    messagequeue[i].put('Done')
-    messagequeue[i].put('Done')
-    messagelock[i].release()
-  for i in range(n_cpu_nodes):
-    workers[i].terminate()
-    workers[i].join()
-    workers[i].close()
-
-  if return_indexer_obj:
-    return dataout,indexer
-  else:
-    return dataout
+  def index_chunk_ray(self,pats=None,indexer=None,patstart=0,npats=-1):
+    try:
+      tic = timer()
+      dataout,indxstart,npatsout = indexer.index_pats(patsin=pats,patstart=patstart,npats=npats,
+                                                      clparams=self.openCLParams)
+      rate = np.array([timer() - tic,npatsout])
+      return dataout,indxstart,indxstart + npatsout,rate
+    except:
+      indxstart = patstart
+      indxend = patstart + npats
+      return None,indxstart,indxend,[-1,-1]
 
 
 class EBSDIndexer():
