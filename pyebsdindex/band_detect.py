3096d4b2277a3c027983649524d0332adab1fd6a
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-02-11 12:48:17 +0100

    Fix PC conventions and add a changelog (#10)
    * Fix PC conventions by indexing correct into NumPy Fortran array
    
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>
    
    * Add a changelog following "Keep a Changelog"
    
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>
    
    * Use float
    
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- pyebsdindex/band_detect.py
+++ pyebsdindex/band_detect.py
@@ -401,7 +401,7 @@ class BandDetect():
     stheta = np.sin(theta)
     ctheta = np.cos(theta)
 
-    t = (np.asfarray(PC)).copy()
+    t = np.asfarray(PC).copy()
     shapet = t.shape
     if len(shapet) < 2:
       t = np.tile(t, nPats).reshape(nPats,3)
@@ -409,16 +409,15 @@ class BandDetect():
       if shapet[0] != nPats:
         t = np.tile(t[0,:], nPats).reshape(nPats,3)
 
-
     dimf = np.array(self.patDim, dtype=np.float32)
-    if (ven in ['EDAX', 'OXFORD']) :
-      t *= np.array([dimf[1],dimf[1],-1.0 * dimf[1]])
-    if (ven == 'EMSOFT'):
-      t += np.array([dimf[1] / 2.0,dimf[0] / 2.0,0.0])
-      t[2,:] *= -1.0
+    if ven in ['EDAX', 'OXFORD']:
+      t *= np.array([dimf[1], dimf[1], -dimf[1]])
+    if ven == 'EMSOFT':
+      t += np.array([dimf[1] / 2.0, dimf[0] / 2.0, 0.0])
+      t[:, 2] *= -1.0
     if ven in ['KIKUCHIPY', 'BRUKER']:
-      t *=  np.array([dimf[1],dimf[0],-1.0 *dimf[0]])
-      t[1,:] = dimf[0] - t[1,:]
+      t *=  np.array([dimf[1], dimf[0], -dimf[0]])
+      t[:, 1] = dimf[0] - t[:, 1]
     # describes the translation from the bottom left corner of the pattern image to the point on the detector
     # perpendicular to where the beam contacts the sample.
 
