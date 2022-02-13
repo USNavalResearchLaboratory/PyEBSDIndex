075f655d75d081bf748995b32befc8e636258759
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-02-13 17:06:17 +0100

    Revert "Add pybind11 to requirements to fix pyopencl install on windows"
    This reverts commit c0f72e5f29fd4ebc496b90f649bb99d8573432aa.

--- setup.py
+++ setup.py
@@ -72,7 +72,6 @@ setup(
         "numpy",
         "numba",
         "pyopencl",
-        "pybind11",
         #"ocl_icd_wrapper_apple;sys_platform == 'darwin'",
         #"ocl-icd-system;sys_platform == 'linux'",
         "pyswarms",
