96da16616af12d84b380e581d788ad57a6ae9b70
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-03-09 17:33:50 +0100

    Set package version to 0.1.0rc1
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- pyebsdindex/__init__.py
+++ pyebsdindex/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Dave Rowenhorst"
 __author_email__ = ""
 # Initial committer first, then sorted by line contributions
-__credits__ = ["Dave Rowenhorst"]
+__credits__ = "Dave Rowenhorst"
 __description__ = "Python based tool for Hough/Radon based EBSD indexing"
 __name__ = "pyebsdindex"
-__version__ = "0.1.dev0"
+__version__ = "0.1.0rc1"
