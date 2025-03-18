b97da7f6bc8bb24a2728ed4bca4858b2e3222134
David Rowenhorst <david.rowenhorst@nrl.navy.mil>
2025-03-18 10:17:53 -0400

    Bumped required Ray, and should remove limits on pydantic.
    Signed-off by: David Rowenhorst <david.rowenhorst@nrl.navy.mil>

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ extra_feature_requirements = {
         "pyopencl",
     ],
     "parallel": [
-        "ray[default]                   >= 1.13",
-        "pydantic                       < 2",
+        "ray[default]                   >= 2.9",
+    #    "pydantic                       < 2",
     ]
 }
 # Create a development installation "dev" including "doc" and "tests"
@@ -57,11 +57,11 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: Other/Proprietary License",
