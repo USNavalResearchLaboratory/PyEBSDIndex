Adjusted stem_scale scaling to sqrt() [not log()].  Corrected method inheritance issues. Signed-off by: David Rowenhorst <david.j.rowenhorst.civ@us.navy.mil>

pyebsdindex/nlpar_cpu.py
@@ -34,6 +34,7 @@ import scipy.optimize as opt
 
 from pyebsdindex import ebsd_pattern
 
+
 #from os import environ
 #environ["NUMBA_CACHE_DIR"] = str(tempdir)
 
@@ -195,9 +196,8 @@ class NLPAR:
     else:
       return None
 
-  def opt_lambda(self,chunksize=0,saturation_protect=True,automask=True, backsub = False,
-                target_weights=(0.5, 0.34, 0.25), dthresh=0.0, autoupdate=True,
-                stem_scale = False,
+  def opt_lambda_cpu(self, target_weights=(0.5, 0.34, 0.25), dthresh=0.0, autoupdate=True,
+                automask = True,
                 verbose = 2, **kwargs):
 
     target_weights = np.asarray(target_weights)
@@ -260,8 +260,7 @@ class NLPAR:
     #   sigma[j:j + rowstartcount[1],:] = tmp
 
 
-    sigma, d2,n2 = self.calcsigma_cpu(chunksize=chunksize, nn=nn,
-                                    saturation_protect = saturation_protect, automask = automask,stem_scale = stem_scale )
+    sigma, d2,n2 = self.calcsigma_cpu(**kwargs)
 
     #print(d2.max(), d2.min())
     #d2 = d2norm(d2, n2, dij, sigma)
@@ -291,7 +290,7 @@ class NLPAR:
       self.sigma = sigma
     return lamopt_values.flatten()
 
-  def calcnlpar(self, chunksize=0, searchradius=None, lam = None, dthresh = None,
+  def calcnlpar_cpu(self, chunksize=0, searchradius=None, lam = None, dthresh = None,
                saturation_protect=True, automask=True, stem_scale = False,
                filename=None, fileout=None, reset_sigma=False, backsub = False, rescale = False,verbose=2,
                diff_offset=None,
@@ -468,8 +467,10 @@ class NLPAR:
                                           convertToFloat=True, returnArrayOnly=True)
         if stem_scale is True:
             datamin = data.min()
-            data = data - datamin + 1
-            data = np.log(data)
+            # data = data - datamin + 1
+            # data = np.log(data)
+            data = data - datamin
+            data = np.sqrt(data)
 
         shpdata = data.shape
 
@@ -500,7 +501,8 @@ class NLPAR:
         dataout = dataout[rstartcalc: rstartcalc + nrowcalc,
                             cstartcalc:cstartcalc + ncolcalc, :]
         if stem_scale is True:
-            dataout = np.exp(dataout) - 1 + datamin
+            #dataout = np.exp(dataout) - 1 + datamin
+            dataout = dataout**2 + datamin
         shpout = dataout.shape
         dataout = dataout.reshape(shpout[0] * shpout[1], pheight, pwidth)
         if rescale == True:
@@ -582,7 +584,8 @@ class NLPAR:
     numba.set_num_threads(nthreadpos)
     return str(patternfileout.filepath)
 
-  def calcsigma(self,chunksize=0,nn=1,saturation_protect=True,automask=True, stem_scale=False,  verbose = 2, **kwargs):
+  def calcsigma_cpu(self,chunksize=0,nn=1,saturation_protect=True,automask=True, stem_scale=False,  verbose = 2, **kwargs):
+
     self.sigmann = nn
     patternfile = self.getinfileobj()
 
@@ -633,9 +636,10 @@ class NLPAR:
                                                 convertToFloat=True, returnArrayOnly=True)
 
             if stem_scale is True:
-                data = data - data.min() + 1
-                data = np.log(data)
-
+                #data = data - data.min() + 1
+                #data = np.log(data)
+                data = data - data.min()
+                data = np.sqrt(data)
             shp = data.shape
             data = data.reshape(data.shape[0], phw)
 
@@ -927,11 +931,15 @@ class NLPAR:
         #print('_______', '\n')
     return dataout
 
-  def opt_lambda_cpu(self,**kwargs): # helper function
-    return self.opt_lambda(**kwargs)
+  def calcsigma(self,**kwargs): # helper function
+    return self.calcsigma_cpu(**kwargs)
 
-  def calcnlpar_cpu(self, **kwargs):  # helper function
-    return self.calcnlpar(**kwargs)
+
+  def opt_lambda(self, **kwargs): # helper function
+    return self.opt_lambda_cpu(**kwargs)
+
+  def calcnlpar(self, **kwargs):  # helper function
+    return self.calcnlpar_cpu(**kwargs)
 
 
   def _calcchunks(self, patdim, ncol, nrow, target_bytes=2e9, col_overlap=0, row_overlap=0, col_offset=0, row_offset=0):

pyebsdindex/opencl/nlpar_cl.py
@@ -56,7 +56,7 @@ class NLPAR(nlpar_cpu.NLPAR):
 
 
   def calcsigma(self,nn=1, saturation_protect=True,automask=True, stem_scale=False,
-                return_nndist=False, **kwargs):
+                   **kwargs):
     self.sigmann = nn
     if self.sigmann > 7:
       print("Sigma optimization search limited to a search radius <= 7")
@@ -64,23 +64,24 @@ class NLPAR(nlpar_cpu.NLPAR):
       nn = 7
       self.sigmann = nn
 
-    sig =  self.calcsigma_cl(nn=nn,
+    sig, dnn, cnn =  self.calcsigma_cl(nn=nn,
                             saturation_protect=saturation_protect,
                             automask=automask,
                             stem_scale = stem_scale,
                             **kwargs)
-    if return_nndist == True:
-      return sig
-    else:
-      return sig[0]
+
+
+    return sig, dnn, cnn
+
   def opt_lambda_cpu(self, **kwargs):
-    return nlpar_cpu.NLPAR.opt_lambda(self, **kwargs)
+    return nlpar_cpu.NLPAR.opt_lambda_cpu(self, **kwargs)
+
 
   def calcnlpar_cpu(self, **kwargs):
-    return nlpar_cpu.NLPAR.calcnlpar(self, **kwargs)
+    return nlpar_cpu.NLPAR.calcnlpar_cpu(self, **kwargs)
 
   def calcsigma_cpu(self,nn=1, saturation_protect=True,automask=True, **kwargs):
-    return nlpar_cpu.NLPAR.calcsigma(self, nn=nn,
+    return nlpar_cpu.NLPAR.calcsigma_cpu(self, nn=nn,
                                      saturation_protect=saturation_protect, automask=automask, **kwargs)
 
   def opt_lambda_cl(self, saturation_protect=True, automask=True, backsub=False,
@@ -139,7 +140,7 @@ class NLPAR(nlpar_cpu.NLPAR):
 
   def calcsigma_cl(self,nn=1,saturation_protect=True,automask=True,
                    stem_scale = False,
-                   normalize_d=False, gpu_id = None, verbose = 2, **kwargs):
+                   normalize_d=True, gpu_id = None, verbose = 2, **kwargs):
     self.sigmann = nn
     if self.sigmann > 7:
       print("Sigma optimization search limited to a search radius <= 7")
@@ -301,10 +302,10 @@ class NLPAR(nlpar_cpu.NLPAR):
         #countnn[rstart:rend, cstart:cend] = countchunk[0:int(ncolchunk*nrowchunk), :].reshape(nrowchunk, ncolchunk, nnn)
         countchunkt = countchunk[0:int(ncolchunk*nrowchunk)].reshape(nrowchunk, ncolchunk, nnn)
         distchunkt = distchunk[0:int(ncolchunk*nrowchunk)].reshape(nrowchunk, ncolchunk, nnn)
-        countnn[rstart:rend, cstart:cend] = np.select([countchunkt >0],
-                                                      [countchunkt], default=countnn[rstart:rend, cstart:cend] )
-        dist[rstart:rend, cstart:cend] = np.select([countchunkt > 0],
-                                                      [distchunkt], default=dist[rstart:rend, cstart:cend])
+        countnn[rstart:rend, cstart:cend,:] = np.select([countchunkt >0],
+                                                      [countchunkt], default=countnn[rstart:rend, cstart:cend,:] )
+        dist[rstart:rend, cstart:cend,:] = np.select([countchunkt > 0],
+                                                      [distchunkt], default=dist[rstart:rend, cstart:cend,:])
 
         #dist[rstart:rend, cstart:cend] = distchunk[0:int(ncolchunk*nrowchunk), :].reshape(nrowchunk, ncolchunk, nnn)
         sigma[rstart:rend, cstart:cend] = np.minimum(sigma[rstart:rend, cstart:cend], sigmachunk)
@@ -318,6 +319,7 @@ class NLPAR(nlpar_cpu.NLPAR):
     queue.flush()
     queue = None
     self.sigma = sigma
+    
     return sigma, dist, countnn
 
 

pyebsdindex/opencl/nlpar_clray.py
@@ -55,7 +55,7 @@ class NLPAR(nlpar_cl.NLPAR):
   def calcnlpar(self, **kwargs):
     return self.calcnlpar_clray(**kwargs)
 
-  def calcsigma(self, nn=1, saturation_protect=True, automask=True, return_nndist=False,
+  def calcsigma(self, nn=1, saturation_protect=True, automask=True, return_nndist=True,
                 stem_scale=False,
                 **kwargs):
     if self.sigmann > 7:
@@ -82,7 +82,7 @@ class NLPAR(nlpar_cl.NLPAR):
     return nlpar_cl.NLPAR.calcsigma_cl(self, **kwargs)
 
   def calcsigma_clray(self, nn=1, saturation_protect=True, automask=True,
-                      stem_scale = False, normalize_d=False,
+                      stem_scale = False, normalize_d=True,
                       gpu_id = None, verbose=2, **kwargs):
     self.patternfile = self.getinfileobj()
     self.sigmann = nn
@@ -741,8 +741,11 @@ class NLPARGPUWorker:
                                                                     [gpujob.ncolchunk, gpujob.nrowchunk]],
                                                                     convertToFloat=False, returnArrayOnly=True)
         if stem_scale == True:
-            data = data - data.min() + 1
-            data = np.log(data)
+            #data = data - data.min() + 1
+            #data = np.log(data)
+            data = data - data.min()
+            data = np.sqrt(data)
+
 
         newdata = nlparobj._sigmachunkcalc_cl(data, gpujob, clparams=self.openCLParams, **kwargs)
 